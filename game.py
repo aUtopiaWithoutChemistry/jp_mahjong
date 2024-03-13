56151edab9e8cb9d20fe51c1837838cc19a9ae3a
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-13 09:21:48 -0400

    added table class and player class

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import random
 from element import all_tiles
 
-# player class，包含属性：点数、手牌、自风、弃牌，函数：摸牌、弃牌、吃、碰、杠、和、立直、加杠
+# player class，包含属性：点数、手牌、自风、弃牌，
+# 函数：摸牌、弃牌、吃、碰、杠、和、立直、加杠
 class player:
     score = 0
     my_tile = []
@@ -11,11 +12,18 @@ class player:
     def __init__(self, score, tile, position):
         self.score = score
         self.my_tile = tile
-
+        self.my_position = position
 
 # table class，包含四个玩家和剩余牌
 class table:
     current_tile = -1
+    total_chang = 0
+    cur_chang = 0
+    cur_ju = 0
+
+    def __init__(self, total_chang):
+        self.total_chang = total_chang
+
 
 
 # generate the shuffled tiles for this game
