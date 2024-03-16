02b1401865a4f408147b8d4f50601ed050b7352e
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-16 14:56:36 -0400

    redesign the tile representation to make following coding easier

--- element.py
+++ element.py
@@ -3,8 +3,8 @@ mahjong_tile_elements = {
     0: '0m', 1: '1m', 2: '2m', 3: '3m', 4: '4m', 5: '5m', 6: '6m', 7: '7m', 8: '8m', 9: '9m',
     10: '0p', 11: '1p', 12: '2p', 13: '3p', 14: '4p', 15: '5p', 16: '6p', 17: '7p', 18: '8p', 19: '9p',
     20: '0s', 21: '1s', 22: '2s', 23: '3s', 24: '4s', 25: '5s', 26: '6s', 27: '7s', 28: '8s', 29: '9s',
-    30: 'dong', 31: 'nan', 32: 'xi', 33: 'bei',
-    40: 'bai', 41: 'fa', 42: 'zhong'}
+    31: 'dong', 32: 'nan', 33: 'xi', 34: 'bei',
+    41: 'bai', 42: 'fa', 43: 'zhong'}
     # using 0, 10, 20 to represent red tile, but when calculating chi, pong, gang,
     # and hu they should be consider as itself + 5
 
