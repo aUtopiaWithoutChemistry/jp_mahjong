705a20ae80c6dd7239b3e6854f50d0a6ff167c0a
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-18 17:56:59 -0400

    write comment for all variable and expressions in element.py

--- element.py
+++ element.py
@@ -1,36 +1,34 @@
-# the value of tile and its corrisponding meaning 
+''' all_tiles are all tiles that can appear on the table, 
+        every single tile in tiles are in the form of (value, id)
+        since have red dora tile, they are evaluated as regular tile during win checking,
+        but have different score in score_counting.
+        So I identify tiles with id 16, 52, 88 are red dora for man, ping, and suo
+
+    mahjong_tile_elements is a dict show every tile's value's meaning in human's word.
+        it might useful when human player playing game, but it will lose meaning when I added
+        graphic user interface. Anyway, it can notice me now about how this number represent
+        mahjong tiles.
+
+    find_num_using_tile is a reversed dict for mahjong_tile_elements, which means people can
+        type in the tile they want to find and get the number of this tile in my representation
+        system. This only useful before I implement the GUI, but I hardly think so that I would 
+        play this game without GUI
+
+    key_list is a helper list for generate all tiles in following for loop, this list contains
+        all the key of mahjong_tile_elements, so every item in this list is the value of four 
+        same tiles.
+
+    using the following for loop, I generated a list of tuples, the list's length is 136 which
+        is the number of all the mahjong tiles, I use the id which is also iterable part in the
+        for loop to // 4 which means I want to let every tiles in key_list generate four tiles 
+        which have the same value but different id. This can help me to identify all red dora
+        tiles without change their value to match other rules, which is a extremly hard problem
+        for jp_mahjong ver -1.1
+'''
+
+all_tiles = []
+
 mahjong_tile_elements = {
-    0: '0m', 1: '1m', 2: '2m', 3: '3m', 4: '4m', 5: '5m', 6: '6m', 7: '7m', 8: '8m', 9: '9m',
-    10: '0p', 11: '1p', 12: '2p', 13: '3p', 14: '4p', 15: '5p', 16: '6p', 17: '7p', 18: '8p', 19: '9p',
-    20: '0s', 21: '1s', 22: '2s', 23: '3s', 24: '4s', 25: '5s', 26: '6s', 27: '7s', 28: '8s', 29: '9s',
-    31: 'dong', 32: 'nan', 33: 'xi', 34: 'bei',
-    41: 'bai', 42: 'fa', 43: 'zhong'}
-    # using 0, 10, 20 to represent red tile, but when calculating chi, pong, gang,
-    # and hu they should be consider as itself + 5
-
-find_num_using_tile = {v: k for k, v in mahjong_tile_elements.items()}
-
-
-# generate all the tiles appear in a single game from all tile elements
-def generate_tiles():
-    all_tiles = []
-    for ele in mahjong_tile_elements:
-        if ele == 0 or ele == 10 or ele == 20:
-            all_tiles.append(ele)
-        elif ele == 5 or ele == 15 or ele == 25: 
-            for i in range(3):
-                all_tiles.append(ele)
-        else:
-            for i in range(4):
-                all_tiles.append(ele)
-    return all_tiles
-
-all_tiles = generate_tiles()
-
-
-all_tiles_new = []
-
-mahjong_tile_elements_new = {
     1: '1m', 2: '2m', 3: '3m', 4: '4m', 5: '5m', 6: '6m', 7: '7m', 8: '8m', 9: '9m',
     11: '1p', 12: '2p', 13: '3p', 14: '4p', 15: '5p', 16: '6p', 17: '7p', 18: '8p', 19: '9p',
     21: '1s', 22: '2s', 23: '3s', 24: '4s', 25: '5s', 26: '6s', 27: '7s', 28: '8s', 29: '9s',
@@ -38,8 +36,10 @@ mahjong_tile_elements_new = {
     41: 'bai', 42: 'fa', 43: 'zhong'
 }
 
-key_list = list(mahjong_tile_elements_new.keys())
+find_num_using_tile = {v: k for k, v in mahjong_tile_elements.items()}
+
+key_list = list(mahjong_tile_elements.keys())
 
 for id in range(136):
     value = key_list[id // 4]
-    all_tiles_new.append((value ,id))
+    all_tiles.append((value ,id))
