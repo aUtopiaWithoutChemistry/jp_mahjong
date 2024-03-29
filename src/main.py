26bd8ec4573a5c647eb951127e2817dbbfb8304e
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-03-28 21:10:48 -0400

    define the besic structure of this game

--- src/main.py
+++ src/main.py
@@ -8,13 +8,12 @@ def main():
      
     # initialize the pygame module
     pygame.init()
+    
     # load and set the logo
     logo = pygame.image.load("../img/logo.jpeg")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Aho Mahjong")
     
-    zero_man = pygame.image.load("../img/0m.png")
-     
     # create a surface on screen that has the size of 1280 x 960
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     background_image = pygame.image.load("../img/start_page.jpeg").convert()
@@ -22,18 +21,15 @@ def main():
     # center the background image
     image_x = (WIDTH - background_image.get_width()) // 2
     image_y = (HEIGHT - background_image.get_height()) // 2
-     
-    # define color
-    color = (0, 204, 204)
     
-    # Create a Rect object (x, y, width, height)
-    rect_obj = pygame.Rect(100, 100, 200, 150)
-    
-    # draw the start button
-    button_rect = pygame.draw.rect(screen, color, rect_obj, width=0, border_radius=3)
+    # control the frame rate
+    clock = pygame.time.Clock()
      
     # define a variable to control the main loop
     running = True
+    
+    # define a variable to control game status
+    game_state = "start"  # start, game, end
     
     # main loop
     while running:
@@ -43,24 +39,41 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-                
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if button_rect.collidepoint(event.pos):
-                # Button clicked! Transition to the game screen.
-                # Change the background image here.
-                    screen.blit()
-                    background_image = pygame.image.load("../img/logo.jpeg")  # Load the new image
-                # Other game setup logic goes here
-                # Set a flag to indicate that the game has started
-                game_started = True
+        
+        # display the background image at the center of the screen
+        screen.blit(background_image, (image_x, image_y))
+        pygame.display.update()
+        
+        # keep the loop running 60 frame per second
+        clock.tick(60)
+        
+        # start page
+        if game_state == "start":
+            ''' in the start page, user can see a big logo with Aho Mahjong and 
+                a menu to select difficulty and game type, then there is a start 
+                button. After user click the start button, the game_state will 
+                change to game.
+            '''
             
-            # Draw the background image
-            # background_image_resized = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-            screen.blit(background_image, (image_x, image_y))
+            pass
+        
+        # game page
+        elif game_state == "game":
+            ''' in the game page, user can see a mahjong table, user's mahjong 
+                tile will display one by one at the bottom of the screen, when 
+                user click one of its tile, the tile will be move up half its 
+                height to show it has been choosen. Every time that user could 
+                have some movement choises like chi, peng, their w
+            '''
+            pass
             
-            # Update the display
-            pygame.display.flip()
-
-   
+        # check point page
+        elif game_state == "check_point":
+            pass
+        
+        # end page
+        elif game_state == "end":
+            pass
+        
 if __name__=="__main__":
     main()
