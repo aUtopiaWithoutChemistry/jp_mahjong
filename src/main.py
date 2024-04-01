19908c28abaccbadcec5cb9f2a867943ca749db6
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2024-04-01 17:36:34 -0400

    added start button and redraw the table for game

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# import the pygame module, so you can use it
+import os
 import pygame
 
 WIDTH, HEIGHT = 1280, 960
@@ -16,17 +16,26 @@ def main():
     
     # create a surface on screen that has the size of 1280 x 960
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen_rect = screen.get_rect()
     
     # load the background image for start page, and convert it to a pygame image object.
-    start_background_image = pygame.image.load("../img/start_page.jpeg").convert()
+    start_background_image = pygame.image.load("../img/start_page.jpeg").convert_alpha()
+    start_background_rect = start_background_image.get_rect(center = (WIDTH // 2, HEIGHT // 2))
     
     # load the background image for game page, and convert it to a pygame image object.
     game_background_image = pygame.image.load("../img/mahjong_table.png").convert()
     game_background_image = pygame.transform.scale(game_background_image, (HEIGHT, HEIGHT)) 
     
-    # center the background image
-    image_x = (WIDTH - start_background_image.get_width()) // 2
-    image_y = (HEIGHT - start_background_image.get_height()) // 2
+    # load tile images
+    tile_images = []
+
+    tiles_folder = '../img/tiles'
+    tile_files = os.listdir(tiles_folder)
+
+    for file in tile_files:
+        file_path = os.path.join(tiles_folder, file)
+        tile_image = pygame.image.load(file_path).convert_alpha()
+        tile_images.append(tile_image)
     
     # create a font
     title_font = pygame.font.Font(None, 100)
@@ -38,7 +47,7 @@ def main():
     running = True
     
     # define a variable to control game status
-    game_state = "game"  # start, game, check_point, end
+    game_state = 'start'  # start, game, check_point, end
     
     # main loop
     while running:
@@ -60,12 +69,31 @@ def main():
                 change to game.
             '''
             # display the background image at the center of the screen
-            screen.blit(start_background_image, (image_x, image_y))
+            screen.blit(start_background_image, start_background_rect)
             
             # display the title at the center of the screen, red color and bold font
             screen.blit(title_font.render("Aho Mahjong", True, 'red'), (WIDTH // 2 - 200, 100))
-            pygame.display.update()
+            
+            # display the start button
+            start_button_surf = title_font.render('Start game!', True, 'black')
+            start_button_rect = start_button_surf.get_rect(center = (WIDTH // 2 + 300, HEIGHT // 2))
+            #pygame.draw.rect(screen, 'white', start_button_rect)
+            pygame.draw.rect(screen, 'white', start_button_rect, border_radius=20)
+            
+            # check if there are mouseclick in this rectangle
+            mouse_pos = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and \
+                start_button_rect.collidepoint(mouse_pos):
+                # change the game_state to game
+                game_state = "game"
+            elif event.type == pygame.MOUSEMOTION and start_button_rect.collidepoint(mouse_pos):
+                # change the color of start button to yellow
+                pygame.draw.rect(screen, 'yellow', start_button_rect, border_radius=20)
         
+            # update the display
+            screen.blit(start_button_surf, start_button_rect)
+            pygame.display.update()
+            
         # game page
         elif game_state == "game":
             ''' in the game page, user can see a mahjong table, user's mahjong 
@@ -78,8 +106,20 @@ def main():
                 at least one player finished hu, then game will change to check
                 point page. 
             '''
+            # clear the screen, black color
+            pygame.draw.rect(screen, 'black', screen_rect)
+            
+            # draw the mahjong table
+            table_rect = pygame.Rect(0, 0, HEIGHT, HEIGHT)
+            inner_rect = pygame.Rect(25, 25, HEIGHT - 50, HEIGHT - 50)
+            inner_color = pygame.Color(79, 164, 133)
+            pygame.draw.rect(screen, inner_color, inner_rect)
+            pygame.draw.rect(screen, 'brown', table_rect, width=25, border_radius=20)
+            
             # display the mahjong table at the left of screen
-            screen.blit(game_background_image, (0, 0))
+            # screen.blit(game_background_image, (0, 0))
+            
+            # update the display
             pygame.display.update()
             
         # check point page
